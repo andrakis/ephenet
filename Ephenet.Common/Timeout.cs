717ae4392e2deaac6ec2d5900934de14e3369620
Julian Thatcher <daedalus.raistlin@gmail.com>
2013-07-16 01:26:32 +1000

    Correct namespaces for Ephenet.Common

--- Ephenet.Common/Timeout.cs
+++ Ephenet.Common/Timeout.cs
@@ -7,7 +7,7 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace Ephenet.Interface
+namespace Ephenet.Common
 {
 	/// <summary>
 	/// Provides an interface similar to EMCA (JavaScript)'s setTimeout.
